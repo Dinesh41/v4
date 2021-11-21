acfacd8b77254adf21b829d99bdc48899e681083
Dinesh41 <dineshsaratha8@gmail.com>
2021-11-21 10:18:28 +0530

    bug fixed

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Dinesh.</h2>;
-  const three = <h3 className="big-heading">I build things for the web and mobile.</h3>;
+  const three = <h3 className="big-heading">I code to build websites and mobile apps.</h3>;
   const four = (
     <>
       <p>
